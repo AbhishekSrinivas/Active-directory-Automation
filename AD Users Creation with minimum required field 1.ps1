35d4270d7d34fc482482e6b8ef0a50ef916a9190
AbhishekSrinivas <42761499+AbhishekSrinivas@users.noreply.github.com>
2021-12-30 23:26:19 +0530

    Add files via upload

---
+++ AD Users Creation with minimum required field 1.ps1
@@ -0,0 +1,46 @@
+﻿# Import active directory module for running AD cmdlets
+#Install-Module AzureAD
+  
+#Store the data from ADUsers.csv in the $ADUsers variable
+$ADUsers = Import-csv C:\Test1.csv
+
+#Loop through each row containing user details in the CSV file 
+foreach ($User in $ADUsers)
+{
+	#Read user data from each field in each row and assign the data to a variable as below
+		
+	$Username 	= $User.username
+	$Password 	= $User.password
+	$Firstname 	= $User.firstname
+	$Lastname 	= $User.lastname
+    $Displayname = $User.Displayname
+	$OU 		= $User.ou #This field refers to the OU the user account is to be created in
+    $email      = $User.email
+    $Password = $User.Password
+
+
+	#Check to see if the user already exists in AD
+	if (Get-ADUser -F {SamAccountName -eq $Username})
+	{
+		 #If user does exist, give a warning
+		 Write-Warning "A user account with username $Username already exist in Active Directory."
+	}
+	else
+	{
+		#User does not exist then proceed to create the new user account
+		
+        #Account will be created in the OU provided by the $OU variable read from the CSV file
+		New-ADUser `
+            -SamAccountName $Username `
+            -UserPrincipalName "$Username@chiko.in" `
+            -Name "$Firstname $Lastname" `
+            -GivenName $Firstname `
+            -Surname $Lastname `
+            -Enabled $True `
+            -DisplayName $Displayname `
+            -Path $OU `
+            -EmailAddress $email `
+            -AccountPassword (convertto-securestring $Password -AsPlainText -Force) -ChangePasswordAtLogon $False -PasswordNeverExpires $True
+            
+	}
+}
